0af6ceac425a193f55e8d29045badd4b4c88d35c
Huzaifa bin Ahmed <65243618+malikhuzaifabinahmed@users.noreply.github.com>
2024-04-26 15:02:12 +0500

    updated the bring us on

--- src/components/BringUsOn.jsx
+++ src/components/BringUsOn.jsx
@@ -5,13 +5,13 @@ import { Button } from "./ui/button";
 export default function BringUsOn() {
   return (
     <div className="bg-background py-10 ">
-      <MaxWidthWrapper className="flex flex-col md:flex-row items-end w-full max-w-[496px]">
+      <MaxWidthWrapper className="flex gap-10 flex-col md:flex-row items-end w-full max-w-[496px]">
         <div className="relative w-full max-w-[753px] aspect-[1.56875] ">
           {" "}
           <Image fill src={"/WordBubbles.png"} />
         </div>
-        <div>
-          <p className="text-[16px] w-full max-w-[496px]">
+        <div className="max-md:mr-auto w-full md:max-w-[496px]">
+          <p className="text-[16px]  ">
             Partner with us and find the people that make things happen. The
             visionaries who unlock growth. Experienced leaders with technical
             backgrounds. Disruptors looking for a new challenge. We&apos;ll help
