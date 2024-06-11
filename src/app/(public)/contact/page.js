fedbee207b7b815b1dbe91167bb36d6aa90faf21
unknown <huzaifamalik3216@gmail.com>
2024-06-11 15:27:33 +0500

    implemented the contact us page\

--- src/app/(public)/contact/page.js
+++ src/app/(public)/contact/page.js
@@ -3,7 +3,7 @@ export default function Page() {
     <div className=" h-[calc(100vh-96px)] mt-[96px]">
       <iframe
         className=" w-full h-full border-none"
-        src="https://jcdqujpmxoo.typeform.com/to/TDman6EV"
+        src="https://jcdqujpmxoo.typeform.com/to/A1l2iv9l"
         width="100%"
         height="100vh"
       ></iframe>
