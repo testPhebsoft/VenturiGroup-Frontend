5bc287ad1538ed32049aefe2f3527ac0a1fa8647
unknown <huzaifamalik3216@gmail.com>
2024-06-11 15:26:18 +0500

    implemented the contact us page\

---
+++ src/app/(public)/contact/page.js
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <div className=" h-[calc(100vh-96px)] mt-[96px]">
+      <iframe
+        className=" w-full h-full border-none"
+        src="https://jcdqujpmxoo.typeform.com/to/TDman6EV"
+        width="100%"
+        height="100vh"
+      ></iframe>
+    </div>
+  );
+}
